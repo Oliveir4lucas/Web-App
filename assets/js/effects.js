bf354d1904ad6aec0af08fec2d6cbfcae9e53017
Lucas Rodrigues <luc4soliveira2018@gmail.com>
2024-03-22 21:29:13 -0300

    Alterações gerais

---
+++ assets/js/effects.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function() {
+    var preloader = document.querySelector('.preloader');
+    var content = document.querySelector('.content');
+
+    // Adiciona a classe 'loaded' após o carregamento completo da página
+    preloader.classList.add('loaded');
+    
+    // Após um pequeno atraso, oculta o preloader e exibe o conteúdo da página
+    setTimeout(function() {
+        preloader.style.display = 'none'; // Oculta o preloader
+        content.style.display = 'block'; // Exibe o conteúdo da página
+    }, 1000); // Ajuste o tempo de atraso conforme necessário
+});
+
+// Adicionando a classe 'loaded' ao corpo da página após o carregamento completo do documento
+window.addEventListener('load', function() {
+    document.body.classList.add('loaded');
+});
+
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("aberto");
+}
